da9cf4546bacd2f7786cf05da6b73e3a01e749a2
jpl98 <87442969+jpl98@users.noreply.github.com>
2022-02-14 09:27:02 -0800

    Cleaned up unnecessary code

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
  import IOSPicker from './picker';
  import Menu from './menu';
  import React, {Component, useState, useEffect} from 'react';
  import {Platform, StyleSheet,Dimensions, Text,Button, View, Alert,Image} from 'react-native';
  import NativeCamera from './NativeCameraModule/NativeCameraModule'
- /* import NativeCameraModule from './NativeCameraModuleNativeModule' */
- 
-
 
 
  const screen = Dimensions.get("screen");
@@ -31,74 +19,22 @@
    constructor(props){
      super(props)
      this.image = null
-   
-
    }
  
- 
-  //  _onPress = () => {
-  //    if(this.camera) {
-  //      this.camera._onTakePhoto()
-  //    }
-  //  }
- 
-  //  _newImageTaken = (e) => {
-  //    this.image = e.nativeEvent.image
-  //    Alert.alert(
-  //      "hi",
-  //      'My Alert Msg',
-  //      [
-  //        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
-  //        {
-  //          text: 'Cancel',
-  //          onPress: () => console.log('Cancel Pressed'),
-  //          style: 'cancel',
-  //        },
-  //        {text: 'OK', onPress: () => console.log('OK Pressed')},
-  //      ],
-  //      {cancelable: false},
-  //    );
-  //    this.forceUpdate()
-  //  }
- 
- 
    render() {
-
-
         
      return (
        <View style={styles.container}>
-
-        
-         {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
-         {
-           this.image ? <Image style={{width: 50, height: 50}} source={{uri: this.image}}/> : null
-         }
-         <Text style={styles.instructions}>hi</Text>
-         <Button onPress={() => {this._onPress()}} title="take a photo"></Button>
-          */}
-         
-        
-       
-
-        
+      
           <NativeCamera  
            onImageReturn={this._newImageTaken}
            ref={e=>{this.camera = e}}
-           style={styles.wrapper} >
-              
-
-         </NativeCamera> 
-{/* 
-        <View style={styles.container2}>
-           <Menu />
-         </View>   */}
+           style={styles.camera} >
+          </NativeCamera> 
          
          <View style={styles.scrollviewGeneral}>
            <IOSPicker />
          </View>
-
-
         
        </View>
      );
@@ -123,16 +59,8 @@
      color: '#333333',
      marginBottom: 5,
    },
-   wrapper: {
-     /* flex: 1,
-     width: '100%',
-     height: '100%', */
-    //  flex:1,
-    //  alignItems: 'stretch',
-    //  flexDirection: 'column',
-    //  justifyContent: 'center',
-    //  borderWidth: 1,
-    height: screen.height,
+   camera: {
+       height: screen.height,
     width: screen.width,
     backgroundColor: 'rgba(150, 150, 150, 0)',
    },
