3fbdad91b3805d1b1db4e50501730eedffe7c431
jpl98 <87442969+jpl98@users.noreply.github.com>
2022-02-15 01:13:45 -0300

    Removed unused stylesheet presets

--- App.js
+++ App.js
@@ -40,127 +40,18 @@
    container: {
     height: screen.height
    },
-   welcome: {
-     fontSize: 20,
-     textAlign: 'center',
-     margin: 10,
-   },
-   instructions: {
-     textAlign: 'center',
-     color: '#333333',
-     marginBottom: 5,
-   },
+  
    camera: {
        height: screen.height,
     width: screen.width,
     backgroundColor: 'rgba(150, 150, 150, 0)',
    },
 
-  icons: {
-    flexDirection: 'row'
-  },
-
-  camera:{
-    height:screen.height
-  },
-
   scrollviewGeneral:{
     flex:1,
     flexDirection: 'row',
     backgroundColor: 'rgba(150, 150, 150, 0)',
     bottom: 500
-  },
-
-  scrollviewAperture:{
-    top: screen.height/1.7,
-    left: screen.width*0.11,
-    width:100,
-  },
-
-  scrollviewISO:{
-    top: screen.height/1.7,
-    left: 38,
-    width:113,
-  },
-
-  scrollviewShutter:{
-    top: screen.height/1.7,
-    left: 30,
-    width:150,
-  },
-
-
-  container2: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'rgba(52, 52, 52, 0)',
-    top: screen.height/1.2,
-    width: 5000
-    
-  },
-
-  text:{
-    fontSize: 20,
-    color: 'rgba(52, 52, 52, 0)',
-    textShadowColor: '#fff',
-    textShadowRadius: 5,
-    top: screen.height/5
-  },
-
- 
-  ISOUp: {
-    width: 40,
-    height: 40,
-    top: screen.height/1.59,
-    left: screen.width/5.2 
-
-  },
-
-  ISODown: {
-    alignSelf: "center",
-    width: 40,
-    height: 40,
-    top: screen.height/1.15,
-    left: screen.width/2-20 - 40 
-
-  },
-
-  apertureUp: {
-    alignSelf: "center",
-    width: 40,
-    height: 40,
-    top: screen.height/1.59,
-    left: screen.width/2.75
-    
-  },
-
-  apertureDown: {
-    alignSelf: "center",
-    flex: 1,
-    width: 40,
-    height: 40,
-    top: screen.height/1.15,
-    left: screen.width/2-18
-
-  },
-
-  shutterSpeedUp: {
-    alignSelf: "center",
-    width: 40,
-    height: 40,
-    top: screen.height/1.59,
-    left: screen.width/1.92
-
-  },
-
-  shutterSpeedDown: {
-    alignSelf: "center",
-    width: 40,
-    height: 40,
-    top: screen.height/1.15,
-    left: screen.width/1.5
-
-
   },
  });
  
