b321406e164167d9609148eceb608711ffc4d96b
jpl98 <87442969+jpl98@users.noreply.github.com>
2022-02-15 22:23:48 -0800

    Facilitated communication between picker and native methods

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
- //Any absolute value written in the stylesheet is a placeholder, meant to be replaced in the near future
-
-
- import IOSPicker from './picker';
+ import { Picker } from '@react-native-picker/picker';
  import React, {Component, useState, useEffect} from 'react';
- import {Platform, StyleSheet,Dimensions, Text,Button, View, Alert,Image} from 'react-native';
+ import {StyleSheet, Dimensions, View} from 'react-native';
  import NativeCamera from './NativeCameraModule/NativeCameraModule'
+
 
  const screen = Dimensions.get("screen");
 
@@ -14,23 +12,102 @@
    constructor(props){
      super(props)
      this.image = null
+     this.state = {
+      ISOValue: 400,
+      shutterValue: 1/250,
+      apertureValue: 5.6
+    }
+
    }
- 
+  
    render() {
         
      return (
        <View style={styles.container}>
-      
-          <NativeCamera  
-           onImageReturn={this._newImageTaken}
-           ref={e=>{this.camera = e}}
-           style={styles.camera} >
-          </NativeCamera> 
-         
-         <View style={styles.scrollviewGeneral}>
-           <IOSPicker />
-         </View>
-        
+
+          
+        <NativeCamera  
+          ref={e=>{this.camera = e}}
+          style={styles.camera} >  
+        </NativeCamera> 
+
+        <View style={styles.scrollviewGeneral}>
+          <Picker
+            style={styles.scrollviewISO}
+            selectedValue={ this.state.ISOValue }
+            onValueChange={(itemValue, itemIndex) => {
+              this.setState({ ISOValue: itemValue});
+              //calls native method
+              this.camera._onISOChange(itemValue)}
+          }>
+            
+            <Picker.Item label='1600' value={1600} color='white' />
+            <Picker.Item label='800' value={800} color='white' />
+            <Picker.Item label='400' value={400} color='white' />
+            <Picker.Item label='200' value={200} color='white' />
+            <Picker.Item label='160' value={160} color='white' />
+            <Picker.Item label='100' value={100} color='white' />
+            <Picker.Item label='50' value={50} color='white' />
+          </Picker>
+
+          <Picker
+            style={styles.scrollviewAperture}
+            selectedValue={ this.state.apertureValue}
+            onValueChange={(itemValue, itemIndex) => {
+              this.setState({ apertureValue: itemValue});
+              /*this.camera._onISOChange(itemValue)*/}
+          }>
+   
+            <Picker.Item label='1' value={1} color='white' />
+            <Picker.Item label='1.4' value={1.4} color='white' />
+            <Picker.Item label='2' value={2} color='white' />
+            <Picker.Item label='2.8' value={2.8} color='white' />
+            <Picker.Item label='4' value={4} color='white' />
+            <Picker.Item label='5.6' value={5.6} color='white' />
+            <Picker.Item label='8' value={8} color='white' />
+            <Picker.Item label='11' value={11} color='white' />
+            <Picker.Item label='16' value={16} color='white' />
+            <Picker.Item label='22' value={22} color='white' />
+            <Picker.Item label='32' value={32} color='white' />
+            <Picker.Item label='45' value={45} color='white' />
+            <Picker.Item label='64' value={64} color='white' />
+            <Picker.Item label='90' value={90} color='white' />
+            <Picker.Item label='128' value={128} color='white' />
+          </Picker>   
+
+          <Picker
+            style={styles.scrollviewShutter}
+            selectedValue={ this.state.shutterValue }
+            onValueChange={(itemValue, itemIndex) => {
+              this.setState({ shutterValue: itemValue});
+              this.camera._onShutterChange(itemValue)}
+          }>
+   
+            {/* <Picker.Item label='500s' value={500} color='white' />
+            <Picker.Item label='250s' value={250} color='white' />
+            <Picker.Item label='125s' value={125} color='white' />
+            <Picker.Item label='60s' value={60} color='white' />
+            <Picker.Item label='30s' value={30} color='white' />
+            <Picker.Item label='15s' value={15} color='white' />
+            <Picker.Item label='8s' value={8} color='white' />
+            <Picker.Item label='4s' value={4} color='white' />
+            <Picker.Item label='2s' value={2} color='white' /> */}
+            <Picker.Item label='1s' value={1} color='white' />
+            <Picker.Item label='1/2' value={2} color='white' />
+            <Picker.Item label='1/4' value={4} color='white' />
+            <Picker.Item label='1/8' value={8} color='white' />
+            <Picker.Item label='1/15' value={15} color='white' />
+            <Picker.Item label='1/30' value={30} color='white' />
+            <Picker.Item label='1/60' value={60} color='white' />
+            <Picker.Item label='1/125' value={125} color='white' />
+            <Picker.Item label='1/250' value={250} color='white' />
+            <Picker.Item label='1/500' value={500} color='white' />
+            <Picker.Item label='1/1000' value={1000} color='white' />
+            <Picker.Item label='1/2000' value={2000} color='white' />
+          </Picker>   
+
+        </View>
+
        </View>
      );
    }
@@ -40,18 +117,39 @@
    container: {
     height: screen.height
    },
-  
+
    camera: {
-       height: screen.height,
+    height: screen.height,
     width: screen.width,
     backgroundColor: 'rgba(150, 150, 150, 0)',
    },
 
   scrollviewGeneral:{
-    flex:1,
+    
+    width: screen.width/3.18,
+    flex: 1,
     flexDirection: 'row',
-    backgroundColor: 'rgba(150, 150, 150, 0)',
-    bottom: 500
+    bottom: 0,
+    left: screen.width/40
+
   },
+
+  scrollviewISO:{
+    width: screen.width/3.18,
+    right: 0
+  },
+
+  scrollviewAperture: {
+    right: 0,
+    
+    width: screen.width/3.18,
+  },
+
+  scrollviewShutter: {
+    right: 0,
+    
+    width: screen.width/3.18,
+  }
+
  });
  
